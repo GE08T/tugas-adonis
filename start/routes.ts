3f43cf9d1f782aa38a161ae420ea7dcb05c5bf1e
GE08T <gaerthamgk@gmail.com>
2023-01-30 21:12:44 +0700

    installing depedencies

--- start/routes.ts
+++ start/routes.ts
@@ -20,6 +20,6 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/', async () => {
-  return 'Hello world from a slim app'
+Route.get('/', async ({ view }) => {
+  return view.render('guest')
 })
